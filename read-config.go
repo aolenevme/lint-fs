396c7c592c5cc349aebe55708a5d8ba4b9c16e76
Alexander Olenev <olenev.biz@gmail.com>
2021-05-06 22:29:16 +0300

    feat/read-config

---
+++ read-config.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+
+	"gopkg.in/yaml.v2"
+)
+
+type Config struct {
+	Ignore []string `yaml:"ignore"`
+	Rules  []string `yaml:"rules"`
+}
+
+func readConfig() {
+	configFile, err := ioutil.ReadFile("./fs-regex.yaml")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var config Config
+
+	err = yaml.Unmarshal(configFile, &config)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+	//nolint
+	fmt.Printf("%+v\n\n", config)
+}
