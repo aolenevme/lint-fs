cd8d864a80fcfc45d36ee4fa80fedd62b1bd4f2f
Alexander Olenev <olenev.biz@gmail.com>
2021-05-06 16:02:52 +0300

    feat/init

--- main.go
+++ main.go
@@ -1,18 +1,19 @@
 package main
 
 import (
-    "fmt"
-    "io/ioutil"
-     "log"
+  "fmt"
+  "io/ioutil"
+  "log"
 )
 
 func main() {
-    files, err := ioutil.ReadDir("./")
-    if err != nil {
-        log.Fatal(err)
-    }
- 
-    for _, f := range files {
-            fmt.Println(f.Name())
-    }
+  files, err := ioutil.ReadDir("./")
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  for _, f := range files {
+    //nolint
+    fmt.Println(f.Name())
+  }
 }
