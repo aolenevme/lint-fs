9c03eb583f88d01a9e262701c5bc1ccf87b1716d
Александр Оленев <a.olenev@city-mobil.ru>
2021-05-06 15:08:52 +0300

    feat/init: Check githooks

--- main.go
+++ main.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-    "fmt"
-    "io/ioutil"
-     "log"
+	"fmt"
+	"io/ioutil"
+	"log"
 )
 
 func main() {
-    files, err := ioutil.ReadDir("./")
-    if err != nil {
-        log.Fatal(err)
-    }
- 
-    for _, f := range files {
-            fmt.Println(f.Name())
-    }
+	files, err := ioutil.ReadDir("./")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, f := range files {
+		fmt.Println(f.Name())
+	}
 }
