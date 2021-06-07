c4950af718be31de8cb8609b61252a628bb19d21
Александр Оленев <a.olenev@city-mobil.ru>
2021-06-07 14:55:01 +0300

    feat/deploy-npm: Managed binary structure

--- cross-build.sh
+++ cross-build.sh
@@ -16,12 +16,16 @@ do
     platform_split=(${platform//\// })
     GOOS=${platform_split[0]}
     GOARCH=${platform_split[1]}
-    output_name="lint-fs-"'-'$GOOS'-'$GOARCH
+
+    directory_name=$GOOS'-'$GOARCH
+    output_name=$directory_name
+
     if [ $GOOS = "windows" ]; then
         output_name+='.exe'
     fi
 
-    env GOOS=$GOOS GOARCH=$GOARCH go build -o bin/$output_name $package
+    mkdir bin/$directory_name
+    env GOOS=$GOOS GOARCH=$GOARCH go build -o bin/$directory_name/$output_name $package
     if [ $? -ne 0 ]; then
         echo 'An error has occurred! Aborting the script execution...'
         exit 1
--- npm/getBinary.js
+++ npm/getBinary.js
@@ -5,11 +5,11 @@ function getPlatform() {
     const type = os.type();
     const arch = os.arch();
 
-    if (type === 'Windows_NT' && arch === 'x64') return 'lint-fs--windows-amd64';
-    if (type === 'Windows_NT') return 'lint-fs--windows-386';
-    if (type === 'Linux' && arch === 'x64') return 'lint-fs--linux-amd64';
-    if (type === 'Linux') return 'lint-fs--linux-386';
-    if (type === 'Darwin' && arch === 'x64') return 'lint-fs--darwin-amd64';
+    if (type === 'Windows_NT' && arch === 'x64') return 'windows-amd64';
+    if (type === 'Windows_NT') return 'windows-386';
+    if (type === 'Linux' && arch === 'x64') return 'linux-amd64';
+    if (type === 'Linux') return 'linux-386';
+    if (type === 'Darwin' && arch === 'x64') return 'darwin-amd64';
 
     throw new Error(`Unsupported platform: ${type} ${arch}`);
 }
