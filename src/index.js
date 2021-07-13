c33c95d2460f09795c8544e8eaaf1f6d5df853a9
Александр Оленев <a.olenev@city-mobil.ru>
2021-07-13 08:13:00 +0300

    feat/rewrite-node-pkg: read yaml file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log('Hey');
+const fs = require('fs');
+const util = require('util');
+const yaml = require('js-yaml');
+
+const readFile = util.promisify(fs.readFile);
+
+async function readConfig() {
+  const readYamlFile = await readFile('./lint-fs.yaml', 'utf8');
+
+  return yaml.load(readYamlFile);
+}
+
+(async () => {
+  const yaml = await readConfig();
+  console.log(yaml);
+})();
