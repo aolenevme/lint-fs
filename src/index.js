e82f7ec70e3d54dfa5321e7b175e0f5a26510143
Александр Оленев <a.olenev@city-mobil.ru>
2021-07-13 08:17:50 +0300

    feat/rewrite-node-pkg: lintFs function

--- src/index.js
+++ src/index.js
@@ -10,7 +10,19 @@ async function readConfig() {
   return yaml.load(readYamlFile);
 }
 
+function lintFs() {
+  let isFsCorrect = true;
+
+  console.log('\n====================\n  Filesystem lint  \n====================\n\n');
+
+  if (!isFsCorrect) {
+    console.error('\n\nFilesystem structure is not correct!\n\n');
+  }
+}
+
 (async () => {
   const yaml = await readConfig();
   console.log(yaml);
+
+  lintFs();
 })();
