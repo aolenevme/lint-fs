bf292f6f46d3044cfcf56d99536ec0f13e9a1cec
Александр Оленев <a.olenev@city-mobil.ru>
2021-07-13 08:39:48 +0300

    feat/rewrite-node-pkg: write more

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const util = require('util');
 const yaml = require('js-yaml');
 
+const readDir = util.promisify(fs.readdir);
 const readFile = util.promisify(fs.readFile);
 
 async function readConfig() {
@@ -10,19 +11,33 @@ async function readConfig() {
   return yaml.load(readYamlFile);
 }
 
-function lintFs() {
+async function lintFs(config) {
   let isFsCorrect = true;
 
   console.log('\n====================\n  Filesystem lint  \n====================\n\n');
+
+  await recursiveLintFs('./', config, isFsCorrect)
 
   if (!isFsCorrect) {
     console.error('\n\nFilesystem structure is not correct!\n\n');
   }
 }
 
-(async () => {
-  const yaml = await readConfig();
-  console.log(yaml);
+async function recursiveLintFs(prevPath, config, isFsCorrect) {
+  const files = await readDir(prevPath);
 
-  lintFs();
+  for (let file of files) {
+    curFilePath = prevPath + file;
+    curDirPath = curFilePath + '/';
+
+    /**
+      isCurFileIgnored = isMatched(config.ignores, curFilePath);
+      isCurDirIgnored = isMatched(config.ignores, curDirPath);
+    **/
+  }
+}
+
+(async () => {
+  const config  = await readConfig();
+  lintFs(config);
 })();
