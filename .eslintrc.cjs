19286085577e8ec179515cbb6d2712992ec0e8fa
Александр Оленев <a.olenev@city-mobil.ru>
2021-07-17 23:03:28 +0300

    feat/rewrite-node-pkg: fixing eslint x3

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,12 +4,5 @@ module.exports = {
     node: true
   },
   extends: ["hardcore", "hardcore/fp", "hardcore/node"],
-  overrides: [
-    {
-      extends: ["hardcore/ts-for-js"],
-      files: ["*.js"],
-      parserOptions: { "project": "./tsconfig.json" }
-    }
-  ],
   parser: "@babel/eslint-parser"
 };
