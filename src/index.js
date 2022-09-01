1cf2a71f13090597cc4e2eb3853b0675f5be5fb1
Alexander Olenev <hello@aolenev.me>
2022-09-02 02:58:59 +0300

    linter x3

--- src/index.js
+++ src/index.js
@@ -1,75 +1,63 @@
-const fs = require('node:fs/promises');
-const yaml = require('js-yaml');
+import fs from 'node:fs/promises';
+import {
+  load,
+} from 'js-yaml';
 
-function abortOnError (error) {
-  console.error(`\n${error}\n\n`);
-
-  process.exit(1);
-}
-
-function isMatched (regExpTemplates, path) {
+const isMatched = (regExpTemplates, path) => {
   return regExpTemplates?.reduce((accumulator, returnValue) => {
     const regExp = new RegExp(returnValue, 'u');
 
     return regExp.test(path) || accumulator;
   }, false);
-}
+};
 
-function getMatchEmoji (im) {
+const getMatchEmoji = (im) => {
   return im ? '✅' : '❌';
-}
+};
 
-async function lintFs (previousPath, config) {
+const lintFs = async (previousPath, config) => {
   const files = await fs.readdir(previousPath);
 
-  files.forEach(async (file) => {
-    try {
-      const currentFilePath = previousPath + file;
-      const currentDirectoryPath = `${currentFilePath}/`;
-      const isCurrentFileIgnored = isMatched(config.ignores, currentFilePath);
-      const isCurrentDirectoryIgnored = isMatched(
-        config.ignores,
-        currentDirectoryPath,
-      );
-      const isDirectory = (await fs.stat(currentFilePath)).isDirectory();
+  for (const file of files) {
+    const currentFilePath = previousPath + file;
+    const currentDirectoryPath = `${currentFilePath}/`;
+    const isCurrentFileIgnored = isMatched(config.ignores, currentFilePath);
+    const isCurrentDirectoryIgnored = isMatched(
+      config.ignores,
+      currentDirectoryPath,
+    );
+    const isDirectory = (await fs.stat(currentFilePath)).isDirectory();
 
-      if (isDirectory && !isCurrentDirectoryIgnored) {
-        await lintFs(currentDirectoryPath, config);
-      }
-
-      if (!isDirectory && !isCurrentFileIgnored) {
-        const isCurrentFileMatched = isMatched(config.rules, currentFilePath);
-
-        console.log(
-          `${currentFilePath} ${getMatchEmoji(isCurrentFileMatched)}`,
-        );
-
-        if (!isCurrentFileMatched) {
-          abortOnError(`Incorrect ${currentFilePath}`);
-        }
-      }
-    } catch (error) {
-      abortOnError(error);
+    if (isDirectory && !isCurrentDirectoryIgnored) {
+      await lintFs(currentDirectoryPath, config);
     }
-  });
-}
 
-async function readConfig () {
+    if (!isDirectory && !isCurrentFileIgnored) {
+      const isCurrentFileMatched = isMatched(config.rules, currentFilePath);
+
+      console.log(
+        `${currentFilePath} ${getMatchEmoji(isCurrentFileMatched)}`,
+      );
+
+      if (!isCurrentFileMatched) {
+        throw new Error(`\nIncorrect ${currentFilePath}\n\n`);
+      }
+    }
+  }
+};
+
+const readConfig = async () => {
   const configFile = await fs.readFile('./lint-fs.yaml', 'utf8');
 
-  return yaml.load(configFile);
-}
+  return load(configFile);
+};
 
 (async () => {
   console.log(
     '====================\n  Filesystem lint  \n====================\n',
   );
 
-  try {
-    const config = await readConfig();
+  const config = await readConfig();
 
-    await lintFs('./', config);
-  } catch (error) {
-    abortOnError(error);
-  }
+  await lintFs('./', config);
 })();
