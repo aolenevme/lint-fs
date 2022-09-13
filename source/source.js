a88bed11d336e977c34f6902189ca55e222382b3
Alexander Olenev <hello@aolenev.me>
2022-09-14 00:36:55 +0300

    brainstorming of logger

--- source/source.js
+++ source/source.js
@@ -4,7 +4,18 @@ import yaml from 'js-yaml';
 import drivers from './drivers/drivers.js';
 import useCases from './useCases/useCases.js';
 
-console.log(await useCases(drivers({
+const {
+  config,
+  filesystem,
+  logger,
+} = drivers({
   fs,
+  std: console,
   yaml,
-})).lintFs());
+});
+
+// 3. улучшить здесь
+logger.log(await useCases({
+  config,
+  filesystem,
+}).lintFs());
