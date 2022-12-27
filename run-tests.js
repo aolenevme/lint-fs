3d546cc025d8743f66f357944f1b6926a81522e1
Alexander Olenev <hello@aolenev.me>
2022-12-28 00:22:42 +0200

    promise rules

--- run-tests.js
+++ run-tests.js
@@ -1,30 +1,31 @@
 /* c8 ignore start */
 import childProcess from 'node:child_process';
+import util from 'node:util';
 
-childProcess.exec(
-  'find ./source -name \'*.test.js\'',
-  (error, stdout, stderr) => {
-    if (error) {
-      throw error;
+const exec = util.promisify(childProcess.exec);
+
+const runTests = async () => {
+  const {
+    stdout: findStdOut,
+    stderr: findStdError,
+  } = await exec('find ./source -name \'*.test.js\'');
+
+  if (findStdError) {
+    throw new Error(findStdError);
+  }
+
+  const testPathes = findStdOut.trim().split('\n');
+
+  for (const path of testPathes) {
+    const {
+      stderr: testStderr,
+    } = await exec(`node ${path}`);
+
+    if (testStderr) {
+      throw new Error(testStderr);
     }
+  }
+};
 
-    if (stderr) {
-      throw new Error(stderr);
-    }
-
-    const testPathes = stdout.trim().split('\n');
-
-    for (const path of testPathes) {
-      childProcess.exec(`node ${path}`, (testError, testStdout, testStderr) => {
-        if (testError) {
-          throw testError;
-        }
-
-        if (testStderr) {
-          throw new Error(testStderr);
-        }
-      });
-    }
-  },
-);
+runTests();
 /* c8 ignore stop */
