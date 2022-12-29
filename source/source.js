24c41b614b7463cb41a2820fd7266d689237bdb3
Alexander Olenev <hello@aolenev.me>
2022-12-29 10:30:40 +0200

    removed runtime type checks

--- source/source.js
+++ source/source.js
@@ -3,7 +3,6 @@ import fs from 'node:fs/promises';
 import yaml from 'js-yaml';
 import drivers from './drivers/drivers.js';
 import useCases from './useCases/useCases.js';
-import './utils/core/core.js';
 
 const {
   config,
--- source/utils/core/core.js
+++
@@ -1,7 +0,0 @@
-Object.prototype.toString = () => {
-  throw new TypeError('toString() is forbidden');
-};
-
-Object.prototype.valueOf = () => {
-  throw new TypeError('valueOf() is forbidden');
-};
