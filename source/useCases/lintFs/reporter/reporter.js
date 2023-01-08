562c74513629a4448d99865ad4b32d62fa8b2fc4
Alexander Olenev <hello@aolenev.me>
2023-01-08 14:45:52 +0200

    minor reporter utils

--- source/useCases/lintFs/reporter/reporter.js
+++ source/useCases/lintFs/reporter/reporter.js
@@ -1,9 +1,5 @@
 import utils from '../../../utils/utils.js';
 
-const {
-  errors,
-} = utils;
-
 const reporter = {
   print (logger, {
     correct,
@@ -15,7 +11,7 @@ const reporter = {
       , titleError,
     ] = logger.log('\u001B[4m\u001B[36m%s\u001B[0m', 'File System is Linted!📐\n');
     if (titleError) {
-      return errors.wrap('reporter', titleError);
+      return utils.errors.wrap('reporter', titleError);
     }
 
     if (mode === 'verbose' && correct.length !== 0) {
@@ -23,14 +19,14 @@ const reporter = {
         , correctTitleError,
       ] = logger.log('\u001B[42m%s\u001B[0m', 'Correct Files');
       if (correctTitleError) {
-        return errors.wrap('reporter', correctTitleError);
+        return utils.errors.wrap('reporter', correctTitleError);
       }
 
       const [
         , correctError,
       ] = logger.logBatch('\u001B[32m%s\u001B[0m', correct);
       if (correctError) {
-        return errors.wrap('reporter', correctError);
+        return utils.errors.wrap('reporter', correctError);
       }
     }
 
@@ -39,14 +35,14 @@ const reporter = {
         , incorrectTitleError,
       ] = logger.log('\u001B[37m\u001B[41m%s\u001B[0m', '\nIncorrect Files');
       if (incorrectTitleError) {
-        return errors.wrap('reporter', incorrectTitleError);
+        return utils.errors.wrap('reporter', incorrectTitleError);
       }
 
       const [
         , incorrectError,
       ] = logger.logBatch('\u001B[31m%s\u001B[0m', incorrect);
       if (incorrectError) {
-        return errors.wrap('reporter', incorrectError);
+        return utils.errors.wrap('reporter', incorrectError);
       }
     }
 
@@ -55,14 +51,14 @@ const reporter = {
         , excessivesTitleError,
       ] = logger.log('\u001B[37m\u001B[41m%s\u001B[0m', '\nExcessive Rules');
       if (excessivesTitleError) {
-        return errors.wrap('reporter', excessivesTitleError);
+        return utils.errors.wrap('reporter', excessivesTitleError);
       }
 
       const [
         , excessivesError,
       ] = logger.logBatch('\u001B[31m%s\u001B[0m', excessives);
       if (excessivesError) {
-        return errors.wrap('reporter', excessivesError);
+        return utils.errors.wrap('reporter', excessivesError);
       }
     }
 
